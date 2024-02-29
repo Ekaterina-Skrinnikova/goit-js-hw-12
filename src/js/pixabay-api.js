4d931cc42ff78acb2b70819193fbbc8444a674ec
Ekaterina-Skrinnikova <kati.skrinnikova@gmail.com>
2024-02-29 12:20:27 +0200

    ready

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,33 @@
-export default function fetchImages(searchQuery) {
-  const URL_NAME = 'https://pixabay.com/api/';
-  const KEY = '42469409-3f592d4c2a8b117d2f80b97d4';
-  const LINK = `${URL_NAME}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+// export default function fetchImages(searchQuery) {
+//   const URL_NAME = 'https://pixabay.com/api/';
+//   const KEY = '42469409-3f592d4c2a8b117d2f80b97d4';
+//   const LINK = `${URL_NAME}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  return fetch(LINK).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
+//   return fetch(LINK).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+import axios from 'axios';
+
+export default async function fetchImages(searchQuery, amountPage, perPage) {
+  const KEY = '42469409-3f592d4c2a8b117d2f80b97d4';
+  // формуємо параметри запиту
+  const params = new URLSearchParams({
+    key: KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: amountPage,
+    per_page: perPage,
   });
+
+  // отримаємо результат запиту
+  const response = await axios.get(`https://pixabay.com/api/?${params}`);
+
+  // повертаємо данні запиту
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -4,16 +4,17 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchImages from './js/pixabay-api';
 import createCard from './js/render-function';
+import axios from 'axios';
 
 const searchForm = document.querySelector('form');
-
+const loadBtn = document.querySelector('.load-btn');
 const spinner = document.querySelector('span');
 const galleryContainer = document.querySelector('.gallery');
 const optionsLightBox = {
   captionsData: 'alt',
   captionDelay: 250,
 };
-const optionsIziToast = {
+const optionsIziToastSearch = {
   message:
     'Sorry, there are no images matching your search query. Please try again!',
   messageColor: '',
@@ -27,37 +28,124 @@ const optionsIziToast = {
   overlay: true,
   overlayClose: true,
 };
+const optionsIziToastLoadMore = {
+  message: "We're sorry, but you've reached the end of search results.",
+  messageColor: '',
+  messageSize: '25',
+  backgroundColor: '#EF4040',
+  balloon: true,
+  theme: 'light',
+  position: 'topRight',
+  close: true,
+  closeOnEscape: true,
+  closeOnClick: true,
+  overlay: true,
+  overlayClose: true,
+  overlayColor: 'rgba(0, 0, 0, 0.3)',
+  transitionIn: 'fadeInUp',
+};
 const lightBox = new SimpleLightbox('.gallery a', optionsLightBox);
 
-searchForm.addEventListener('submit', onClickBtn);
+const perPage = 15;
+const i = 2;
+let amountPage = 1;
+let searchQuery = '';
 
-function onClickBtn(e) {
+searchForm.addEventListener('submit', onSubmitBtn);
+loadBtn.addEventListener('click', onClickLoadBtn);
+
+hideLoadBtn();
+
+async function onSubmitBtn(e) {
   e.preventDefault();
 
-  const searchQuery = e.currentTarget.elements.query.value;
+  // очищаємо контейнер перед новим запитом
+  resetPage();
+  // отримуємо значення з інпут
+  const searchQuery = e.currentTarget.elements.query.value.trim();
 
   if (searchQuery) {
-    spinner.classList.add('loader');
+    // addLoader();
+    try {
+      // отримуємо об'єкт з картинками
+      const images = await fetchImages(searchQuery, amountPage, perPage);
 
-    fetchImages(searchQuery)
-      .then(data => {
-        if (data.total === 0) {
-          iziToast.error(optionsIziToast);
-        }
+      if (images.total === 0) {
+        resetForm();
+        hideLoadBtn();
+        // deleteLoader();
+        return iziToast.error(optionsIziToastSearch);
+      } else if (images.totalHits < perPage) {
+        addCardMarkup(images.hits);
+        hideLoadBtn();
+        // deleteLoader();
+      } else {
+        // рендеремо картки
+        addCardMarkup(images.hits);
+        visibleLoadBtn();
+        // deleteLoader();
 
-        addCardMarkup(data.hits);
-      })
-      .catch(error => iziToast.error(optionsIziToast));
+        return searchQuery;
+      }
+    } catch (error) {
+      hideLoadBtn();
+      // deleteLoader();
+      iziToast.error(optionsIziToastSearch);
+    }
   }
   resetForm();
 }
 
+async function onClickLoadBtn() {
+  addLoader();
+  try {
+    amountPage += 1;
+    const images = await fetchImages(searchQuery, amountPage, perPage);
+    const totalPage = Math.ceil(images.totalHits / perPage);
+
+    if (amountPage > totalPage) {
+      return iziToast.error(optionsIziToastLoadMore);
+    }
+    addCardMarkup(images.hits);
+
+    // робимо прокрутку екрана пр натисканні кнопки
+    const cardElement = document.querySelector('.gallery-image');
+    // отримуємо за допомогою метода об'єкт з властивостями елемента та вибираємо висоту
+    const heightCard = cardElement.getBoundingClientRect().height;
+    const heightScroll = i * heightCard;
+    window.scrollBy(0, heightScroll);
+  } catch (error) {
+    deleteLoader();
+    iziToast.error(optionsIziToastLoadMore);
+  }
+}
+
 function addCardMarkup(images) {
-  galleryContainer.innerHTML = createCard(images);
-  spinner.classList.remove('loader');
+  galleryContainer.insertAdjacentHTML('beforeend', createCard(images));
+  deleteLoader();
   lightBox.refresh();
 }
 
 function resetForm() {
   searchForm.reset();
+}
+
+function resetPage() {
+  galleryContainer.innerHTML = '';
+}
+
+function hideLoadBtn() {
+  loadBtn.classList.add('is-hidden');
+}
+
+function visibleLoadBtn() {
+  loadBtn.classList.remove('is-hidden');
+}
+
+function addLoader() {
+  spinner.classList.add('loader');
+}
+
+function deleteLoader() {
+  spinner.classList.remove('loader');
 }
